7a1bd19f4d9ccf4589fa261b4c21c28adda7be61
Chris <camargo.cac@gmail.com>
2014-04-18 14:38:09 -0700

    added new files

---
+++ GolfBawlZ/headers/engine/Level.h
@@ -0,0 +1,21 @@
+#ifndef LEVEL_H
+#define LEVEL_H
+
+#include <vector>
+#include <glm.hpp>
+
+using namespace glm;
+
+class Level
+{
+	public:
+		// Constructor.
+		explicit Level();
+
+		// Destructor.
+		~Level();
+
+	private:
+};
+
+#endif
