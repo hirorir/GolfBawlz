5ed177f4ec215f04b479bc7d0f69947ca0699cab
Chris <camargo.cac@gmail.com>
2014-04-22 18:21:34 -0700

    new files

--- GolfBawlZ/src/Camera.cpp
+++
@@ -1,1 +0,0 @@
-#include "../headers/camera.h"
--- GolfBawlZ/src/GolfBawlZ.cpp
+++
@@ -1,9 +0,0 @@
-#include "../headers/GameManager.h"
-
-int main(int argc, char **argv) {
-	GameManager *manager = new GameManager(argc, argv);
-
-	//manager->run();
-
-	return 0;
-}
